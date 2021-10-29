1aaceee7f7c95a4ef607e397b543c6929373bb82
czf <2455501+czf@users.noreply.github.com>
2021-10-29 14:31:31 -0700

    running as service

--- Czf.App.Background.KeepSystemInUse/Program.cs
+++ Czf.App.Background.KeepSystemInUse/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 
 namespace Czf.App.Background.KeepSystemInUse
 {
@@ -15,18 +14,20 @@ namespace Czf.App.Background.KeepSystemInUse
     {
         static async Task Main(string[] args)
         {
+            int minutes = args.Length < 1 ? 7 : int.Parse(args[0]);
+
             await CreateHostBuilder(args)
+                .UseWindowsService(o =>
+                {
+                    o.ServiceName = "Czf.App.Background.KeepSystemInUse";
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
                     var config = hostContext.Configuration;
                     services.AddOptions();
-                    services.AddHostedService(x => new KeepSystemInUseService(int.Parse(args[0]), DefaultScheduler.Instance));
+                    services.AddHostedService(x => new KeepSystemInUseService(x.GetRequiredService<IHostApplicationLifetime>(), minutes, DefaultScheduler.Instance));
                 })
                 .RunConsoleAsync();
-
-
-            //await host.RunAsync();
-
         }
         static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -37,21 +38,32 @@ namespace Czf.App.Background.KeepSystemInUse
                 .AddJsonFile("appsettings.json",true)
                 .AddCommandLine(args);
             });
-        public class KeepSystemInUseService : IHostedService
+
+
+
+        private class KeepSystemInUseService : IHostedService
         {
+            private bool disposedValue;
             private readonly int _intervalMinutes;
             private readonly IScheduler _scheduler;
             private IObservable<long> _intervalObservable;
-            public KeepSystemInUseService(int intervalMinutes, IScheduler scheduler)
+            private CancellationTokenSource _linkedTokenSource;
+            private IHostApplicationLifetime _applicationLifetime;
+            public KeepSystemInUseService(IHostApplicationLifetime applicationLifetime, int intervalMinutes, IScheduler scheduler)
             {
+                _applicationLifetime = applicationLifetime;
                 _intervalMinutes = intervalMinutes;
                 _scheduler = scheduler;
+                _linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(_applicationLifetime.ApplicationStopping, _applicationLifetime.ApplicationStopped);
             }
 
             public Task StartAsync(CancellationToken cancellationToken)
             {
-                _intervalObservable = Observable.Interval(TimeSpan.FromMinutes(_intervalMinutes), _scheduler);
-                _intervalObservable.Subscribe(OnNext, cancellationToken);
+                if (!cancellationToken.IsCancellationRequested)
+                {
+                    _intervalObservable = Observable.Interval(TimeSpan.FromMinutes(_intervalMinutes), _scheduler);
+                    _intervalObservable.Subscribe(OnNext,OnError, _linkedTokenSource.Token);
+                }
                 return Task.CompletedTask;
             }
 
@@ -59,17 +71,48 @@ namespace Czf.App.Background.KeepSystemInUse
             {
                 SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
             }
+
+            public void OnError(Exception e)
+            {
+                Console.Error.WriteLine($"exception: {e.Message}");
+                _applicationLifetime.StopApplication();
+            }
             public Task StopAsync(CancellationToken cancellationToken)
             {
-                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+                try
+                {
+                    SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+                }
+                finally
+                {
+                    _applicationLifetime.StopApplication();
+                }
                 return Task.CompletedTask;
             }
 
             [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
             static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
 
-            [FlagsAttribute]
-            public enum EXECUTION_STATE : uint
+            public void Dispose()
+            {
+                Dispose(disposing: true);
+                GC.SuppressFinalize(this);
+            }
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        _linkedTokenSource.Dispose();
+                    }
+
+                    disposedValue = true;
+                }
+            }
+
+            [Flags]
+            private enum EXECUTION_STATE : uint
             {
                 ES_AWAYMODE_REQUIRED = 0x00000040,
                 ES_CONTINUOUS = 0x80000000,
